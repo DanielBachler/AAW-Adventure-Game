7aaa2dca955d0ba25af1263436577c21db8f82c6
Daniel Bachler <daniel.bachler@comcast.net>
2019-01-18 16:31:39 -0700

    New methods and part of first branch
    Added 2 new methods, one to clean user input for choices, and one to change player height.  Also added the player height attribute.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,4 @@
+package com.company;
 /**
  * Daniel Bachler
  * Choose your own adventure game for LIT-169 Assignment 1
@@ -5,20 +6,30 @@
  * original Alice in Wonderland novel by Lewis Carroll
  */
 
-package com.company;
-
+import java.util.Random;
 import java.util.Scanner;
 
 //Driving class, runs the game
 public class Main {
+    //The random for height generation and other features
+    private static Random randy = new Random();
+    //The scanner for user input
+    private static Scanner userInput = new Scanner(System.in);
 
     public static void main(String[] args) {
         //Creates player object
         Player player = new Player();
         //Intro and gets player name
-        Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to the Alice in Wonderland choose your own adventure game!\nWhats your name?");
         String userName = userInput.nextLine();
+        //Gets player height
+        System.out.println("How tall are you? (ft) Ex. 6.3");
+        float height = userInput.nextFloat();
+        //Fixes control flow
+        userInput.nextLine();
+        //Assigns player height
+        player.setPlayerHeight(height);
+        //Starts game
         System.out.println("The game starts now " + userName + "!");
         //Assigns player name
         player.setPlayerName(userName);
@@ -31,7 +42,14 @@ public class Main {
         System.out.println("You find yourself in a strange room, theres a table laden with various foods and drinks");
         //Option
         System.out.println("(1) Eat or drink something from the table\n(2) Take food or drink from the table");
-
+        String choice1 = userInput.nextLine();
+        //Branch one
+        if(cleanInput(choice1) == 1) {
+            System.out.println("You choose to consume an item off the table");
+            changePlayerHeight(p);
+        } else if(cleanInput(choice1) == 2) {
+            System.out.println();
+        }
     }
 
     //Cleans the input to just an integer
@@ -43,5 +61,28 @@ public class Main {
         } else {
             return 0;
         }
+    }
+
+    //Grows or shrinks the player
+    private static void changePlayerHeight(Player p) {
+        //Decides if growing or shrinking
+        boolean grow = randy.nextBoolean();
+        //Determines amount grown or shrunk
+        double change = randy.nextDouble() + randy.nextInt(4);
+
+        //Decides players new height
+        if(grow) { //Grows
+            p.setPlayerHeight(p.getPlayerHeight() + change);
+        } else { //Shrinks
+            double newHeight = p.getPlayerHeight() - change;
+            //If player would shrink into negative sets height to 0.1ft
+            if(newHeight > 0) {
+                p.setPlayerHeight(newHeight);
+            } else {
+                p.setPlayerHeight(0.1);
+            }
+        }
+        //Gets and displays new character height
+        System.out.printf("Your character %s!\nYour new height is %.2f", (grow? "grew" : "shrunk"), p.getPlayerHeight());
     }
 }
